f80f4f6ff83bea046209d41a326960cff1b94815
Markus Hanses <me@markushanses.de>
2016-06-21 22:29:19 +0200

    Roles reduced to minimum.

--- ansible/roles/jenkins-jobs/templates/sample-maven-config.groovy
+++
@@ -1,8 +0,0 @@
-node {
-    stage "Checkout Repository"
-    git url: 'https://github.com/marhan/jenkins-pipeline-sample.git'
-
-    stage "Build Project"
-    def mvnHome = tool 'M3'
-    sh "${mvnHome}/bin/mvn clean package"
-}
