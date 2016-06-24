fcfdcc6b3eacf1018a03a101e4213aecff8b0f82
Markus Hanses <me@markushanses.de>
2016-06-24 22:48:46 +0200

    Docker role fixed, script added for shutdown, readme improved.

---
+++ scripts/shutdown.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Shutdown RPIs...."
+echo
+
+for ip in "192.168.178.53" "192.168.178.72" "192.168.178.74" "192.168.178.76" "192.168.178.77"
+  do
+    ssh pi@$ip 'sudo shutdown now'
+done
+
+echo
+echo "End of file"
